6b004b29e16d146ea35487a3f241b8a123c22240
Sanjay Gurung <sanjay@gritfeat.com>
2022-05-26 23:20:07 +0545

    Improvement: Add navigation bar

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom'
-import Signup from '../Components/Signup';
-import Welcome from '../Components/Welcome';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Signup from "../Components/Signup";
+import Welcome from "../Components/Welcome";
 
 const NavRouter = () => {
   return (
-      <Routes>
-        <Route path="/" exact element={<Signup />} />
-        <Route path="/welcome/:username" exact element={<Welcome />} />
-      </Routes>
-  )
-}
+    <Routes>
+      <Route path="/" exact element={<Signup />} />
+      <Route path="/welcome/:username" exact element={<Welcome />} />
+    </Routes>
+  );
+};
 
 export default NavRouter;
