031eb996b0def497f4753ba88466f053027f21b6
Sanjay Gurung <sanjay@gritfeat.com>
2022-06-06 23:58:51 +0545

    Improvement: Updated protected router

--- src/Components/Common/ProtectedRoute/index.jsx
+++ src/Components/Common/ProtectedRoute/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 
 export const ProtectedRoute = ({ children }) => {
+  const navigate = useNavigate();
   const auth = localStorage.getItem("twj");
   try {
     if (auth) {
       jwt_decode(JSON.parse(auth).token);
+      if (navigate === "/login") {
+        <Navigate to="/" replace={true} />;
+      }
       return children;
     }
   } catch (err) {
